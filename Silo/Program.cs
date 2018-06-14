c4dbe54678d588da37d261d3d754423c92820e37
Elliot Newnham <darkcow@gmail.com>
2018-06-14 10:47:35 -0400

    Add a Silo

--- Silo/Program.cs
+++ Silo/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Hosting;
 
 namespace Silo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var siloBuilder = new SiloHostBuilder()
+                    .UseLocalhostClustering()
+                    .Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "dev";
+                        options.ServiceId = "Orleans2GettingStarted";
+                    })
+                    .Configure<EndpointOptions>(options =>
+                        options.AdvertisedIPAddress = IPAddress.Loopback)
+                    .ConfigureLogging(logging => logging.AddConsole());
+
+            using (var host = siloBuilder.Build())
+            {
+                await host.StartAsync();
+
+                Console.WriteLine("Press Any Key To Exit...");
+                Console.ReadLine();
+            }
         }
     }
 }
